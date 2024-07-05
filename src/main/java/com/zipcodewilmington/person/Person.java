7ae72bb8607cfbf0460ca24a5b81fc4ededc5d25
ttxlnguyen <ttxl.nguyen@gmail.com>
2024-07-05 09:51:20 -0400

    finished

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -8,28 +8,44 @@ public class Person {
     private int age;
 
     public Person() {
+        name = "";
+        age = Integer.MAX_VALUE;
     }
 
     public Person(int age) {
+        this.age = 5;
     }
 
     public Person(String name) {
+        this.name = "Leon";
     }
 
     public Person(String name, int age) {
+        this.name = "Leon";
+        this.age = 5;
     }
 
+
+    // Constructors Above
+
+
     public void setName(String name) {
+        this.name = "Leon";
     }
 
     public void setAge(int age) {
+        this.age = 5;
     }
 
+
+    // Setters Above ^ Getters Below v
+
+
     public String getName() {
-        return null;
+        return name;
     }
 
     public Integer getAge() {
-        return null;
+        return age;
     }
 }
